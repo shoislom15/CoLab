7d8c3f42c50c5ec70c98285fd66a30bd513bdfc6
qutbiddin <shoislomshobaxromov@gmail.com>
2022-02-10 18:10:28 +0500

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+
+  const [npm, setNpm] = useState("npm i ");
+
+  const lib = [
+    {
+      name: "MUI",
+      text: "@mui/material @emotion/react @emotion/styled"
+    },
+    {
+      name: "Bootstrap",
+      text: "bootstrap",
+    },
+    {
+      name: "React Bootstrap",
+      text: "react-bootstrap",
+    },
+    {
+      name: "Styled Components",
+      text: "styled-components",
+    },
+    {
+      name: "React Router Dom",
+      text: "react-router-dom"
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="row">
+          {lib.map((e) => {
+            return <>
+              <div className="col-lg-3 col-md-4 col-sm-6 p-4">
+                <button onClick={() => {
+                  setNpm(`${npm} ${e.text}`);
+                }} className="btn w-100 bg-primary">{e.name}</button>
+              </div>
+            </>
+          })}
+          <div className="col-12">
+            <button className="w-100 btn btn-danger" onClick={() => {
+              navigator.clipboard.writeText(npm);
+            }}>Copy</button>
+          </div>
+        </div>
+
+      </div>
+    </>
   );
 }
 
